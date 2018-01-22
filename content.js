22d59a2d8c34bc233415be91ada83dd25333aadd
Marcos Felipe <omarkdev@gmail.com>
2018-01-21 23:55:15 -0200

    Create multiples events and create eventAlert

--- content.js
+++ content.js
@@ -6,6 +6,32 @@ document.addEventListener('focusin', function(e) {
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.event == "write") {
-        inputFocused.value = request.value;
+        return eventWrite(request, sender, sendResponse);
+    }
+
+    if (request.event == "alert") {
+        return eventAlert(request, sender, sendResponse);
     }
 });
+
+/**
+ * Execute the event write received from listener.
+ *
+ * @param request
+ * @param sender
+ * @param sendResponse
+ */
+const eventWrite = function(request, sender, sendResponse) {
+    inputFocused.value = request.value;
+};
+
+/**
+ * Execute the event alert received from listener.
+ *
+ * @param request
+ * @param sender
+ * @param sendResponse
+ */
+const eventAlert = function(request, sender, sendResponse) {
+    alert(request.value);
+};
